75d278b34b32e9ae4960126e2384a701c13ca3a9
whitehat-G6 <88480121+WHJR-G6@users.noreply.github.com>
2021-08-24 16:23:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+var ball,img,paddle;
+var bounceSound;
+function preload() {
+  ballimg = loadImage('ball.png');
+  paddleimg=loadImage("paddle.png");
+  bgImage = loadImage("bg.PNG");
+  bounceSound= loadSound("Bounce.mp3");
+}
+
+function setup() {
+  createCanvas(400, 400);
+  ball=createSprite(40,200,20,20);
+  ball.addImage (ballimg); 
+  ball.velocityX=9;  
+  paddle=createSprite(350,200,20,100);
+  paddle.addImage(paddleimg);
+}
+
+function draw() {
+  background(bgImage);
+  edges=createEdgeSprites();
+  //Bounce Off the Left Edge only
+  ball.bounceOff(edges[0]); 
+  
+ ball.bounceOff(edges[2]);
+  ball.bounceOff(edges[3]);
+  ball.bounceOff(paddle,explosion);
+  paddle.collide(edges);
+  if(keyDown(UP_ARROW))
+  {
+    paddle.y=paddle.y-20;
+  }
+  
+  if(keyDown(DOWN_ARROW))
+  {
+    paddle.y=paddle.y+20;
+  }
+  drawSprites();
+  
+}
+
+function explosion()
+{
+  ball.velocityY=random(-8,8);
+
+  bounceSound.play();
+
+}
+
